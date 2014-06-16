0196d116844ea5a5816706d0acc18f875b055771
girish <girish946@gmail.com>
2014-06-16 23:29:48 +0530

    this is first commit

---
+++ type/Type_master.java
@@ -0,0 +1,13 @@
+package type;
+public class Type_master extends Frame
+{
+		public Type_master()
+	{
+		super("Type_master");
+		
+	}
+	public static void main(String arg[])
+	{
+		new Type_master();
+	}
+}
